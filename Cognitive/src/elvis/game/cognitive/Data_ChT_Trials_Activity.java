c92cce27677c61be97d4a35380dadde17b9e0280
Chen <Chen@Elvis-Desktop>
2015-09-03 19:42:14 -0400

    Desktop Initial commit #2

--- Cognitive/src/elvis/game/cognitive/Data_ChT_Trials_Activity.java
+++ Cognitive/src/elvis/game/cognitive/Data_ChT_Trials_Activity.java
@@ -32,25 +32,6 @@ public class Data_ChT_Trials_Activity extends Activity {
 		mgr = new DBManager(this);
 		show();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.data__ch_t__trials_, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 	
 	private void show() {
 		List<Trials> trials = mgr.query();
